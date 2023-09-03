72b4a9424eaa94ae2d39e6f0fbe54ebf98a6428a
BR4YD3N-G <braydengraham@gmail.com>
2023-09-03 12:04:59 -0600

    Initial Commit

---
+++ HealthLink/HealthLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthLinkApp.swift
+//  HealthLink
+//
+//  Created by Brayden Graham on 9/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
